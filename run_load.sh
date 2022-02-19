826cc21391d3780c937a5b9f98cf86c08ebac5bc
KinderRiven <1339764596@qq.com>
2022-02-19 19:57:30 +0800

    hsk:push

--- run_load.sh
+++ run_load.sh
@@ -1,15 +1,19 @@
-clickhouse-client -h 10.10.1.8 --port 9000 --query="DROP TABLE IF EXISTS customer"
+sf=100
+DIR=/mnt/data/ssb-$sf
+ip=10.10.1.8
+port=9000
 
-clickhouse-client -h 10.10.1.8 --port 9000 --query="DROP TABLE IF EXISTS lineorder"
-
-clickhouse-client -h 10.10.1.8 --port 9000 --query="DROP TABLE IF EXISTS part"
-
-clickhouse-client -h 10.10.1.8 --port 9000 --query="DROP TABLE IF EXISTS supplier"
+clickhouse-client -h $ip --port $port --query="DROP TABLE IF EXISTS customer"
+clickhouse-client -h $ip --port $port --query="DROP TABLE IF EXISTS lineorder"
+clickhouse-client -h $ip --port $port --query="DROP TABLE IF EXISTS part"
+clickhouse-client -h $ip --port $port --query="DROP TABLE IF EXISTS supplier"
 
 cat load/single/create_customer_into_single.sql | clickhouse-client -h 10.10.1.8 --port 9000
-
 cat load/single/create_lineorder_into_single.sql | clickhouse-client -h 10.10.1.8 --port 9000
-
 cat load/single/create_part_into_single.sql | clickhouse-client -h 10.10.1.8 --port 9000
-
 cat load/single/create_supplier_into_single.sql | clickhouse-client -h 10.10.1.8 --port 9000
+
+date && time clickhouse-client -h $ip --port $port --query "INSERT INTO customer FORMAT CSV" < $DIR/customer.tbl
+date && time clickhouse-client -h $ip --port $port --query "INSERT INTO part FORMAT CSV" < $DIR/part.tbl
+date && time clickhouse-client -h $ip --port $port --query "INSERT INTO supplier FORMAT CSV" < $DIR/supplier.tbl
+date && time clickhouse-client -h $ip --port $port --query "INSERT INTO lineorder FORMAT CSV" < $DIR/lineorder.tbl
