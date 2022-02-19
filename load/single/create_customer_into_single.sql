1e8e81f77d4ca12956174b9eccc18da5da901a9f
KinderRiven <1339764596@qq.com>
2022-02-19 19:52:30 +0800

    hsk:push

--- load/single/create_customer_into_single.sql
+++ load/single/create_customer_into_single.sql
@@ -1,22 +1,13 @@
-CREATE TABLE lineorder
-(
- LO_ORDERKEY            UInt32,
- LO_LINENUMBER          UInt8,
- LO_CUSTKEY       	    UInt32,
- LO_PARTKEY             UInt32,
- LO_SUPPKEY             UInt32,
- LO_ORDERDATE           Date,
- LO_ORDERPRIORITY       String,
- LO_SHIPPRIORITY        UInt8,
- LO_QUANTITY            UInt8,
- LO_EXTENDEDPRICE       UInt32,
- LO_ORDTOTALPRICE       UInt32,
- LO_DISCOUNT            UInt8,
- LO_REVENUE             UInt32,
- LO_SUPPLYCOST          UInt32,
- LO_TAX                 UInt8,
- LO_COMMITDATE          Date,
- LO_SHIPMODE            sString
-)
-ENGINE = MergeTree PARTITION BY toYear(LO_ORDERDATE) ORDER BY (LO_ORDERDATE, LO_ORDERKEY)
+CREATE TABLE customer                                                                                                       
+(                                                                                                                                         
+ C_CUSTKEY      	UInt32,                                                                                                                   
+ C_NAME           	String,                                                                                                                   
+ C_ADDRESS      	String,                                                                                                                   
+ C_CITY             String,                                                                                                                   
+ C_NATION       	String,                                                                                                                   
+ C_REGION        	String,                                                                                                                   
+ C_PHONE         	String,                                                                                                                   
+ C_MKTSEGMENT    	String                                                                                                                    
+)                                                                                                                                     
+ENGINE = MergeTree ORDER BY (C_CUSTKEY)
 SETTINGS storage_policy = 'only_local'
