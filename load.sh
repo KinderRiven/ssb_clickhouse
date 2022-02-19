2b15176c77699eca19220af19cb11d24ef59abba
KinderRiven <1339764596@qq.com>
2022-02-19 19:51:01 +0800

    hsk:push

--- load.sh
+++ load.sh
@@ -6,10 +6,10 @@ clickhouse-client -h 10.10.1.8 --port 9000 --query="DROP TABLE IF EXISTS part"
 
 clickhouse-client -h 10.10.1.8 --port 9000 --query="DROP TABLE IF EXISTS supplier"
 
-cat load/create_customer_into_single.sql | clickhouse-client -h 10.10.1.8 --port 9000
+cat load/single/create_customer_into_single.sql | clickhouse-client -h 10.10.1.8 --port 9000
 
-cat load/create_lineorder_into_single.sql | clickhouse-client -h 10.10.1.8 --port 9000
+cat load/single/create_lineorder_into_single.sql | clickhouse-client -h 10.10.1.8 --port 9000
 
-cat load/create_part_into_single.sql | clickhouse-client -h 10.10.1.8 --port 9000
+cat load/single/create_part_into_single.sql | clickhouse-client -h 10.10.1.8 --port 9000
 
-cat load/create_supplier_into_single.sql | clickhouse-client -h 10.10.1.8 --port 9000
+cat load/single/create_supplier_into_single.sql | clickhouse-client -h 10.10.1.8 --port 9000
