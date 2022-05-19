b71fabf38d67ea4808d2b258658ed688b54cc84a
KinderRiven <1339764596@qq.com>
2022-05-19 17:57:28 +0800

    hsk:push

--- workloads/run.sh
+++ workloads/run.sh
@@ -2,4 +2,4 @@ ip=10.10.1.8
 port=9000
 
 # time (clickhouse-client -h $ip --port $port --queries-file=workload_A.sql)
-time (clickhouse-client -h $ip --port $port --queries-file=workload_B.sql)
+time (clickhouse-client -h $ip --port $port --queries-file=workload_C.sql)
